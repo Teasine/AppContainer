24a7545d1532b66de0c532edbe27ac871e788f94
santipt <santiperez1199@gmail.com>
2019-09-25 15:57:55 +0200

    mapa integrado

--- AppContainer/app/src/main/java/com/example/container/appcontainer/MainActivity.java
+++ AppContainer/app/src/main/java/com/example/container/appcontainer/MainActivity.java
@@ -3,11 +3,16 @@ package com.example.container.appcontainer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.SupportMapFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
     }
 }
