e3fefb83bea9bf3d811feee89221be7dc59c2de8
santipt <santiperez1199@gmail.com>
2020-01-14 17:20:10 +0100

    logica fake implementada

---
+++ AppContainer/app/src/main/java/com/example/container/appcontainer/CallbackPet.java
@@ -0,0 +1,5 @@
+package com.example.container.appcontainer;
+
+public interface CallbackPet {
+    void callbackCall(String respuesta);
+}
---
+++ AppContainer/app/src/main/java/com/example/container/appcontainer/LogicaFake.java
@@ -0,0 +1,46 @@
+// .....................................................................
+// Autor: Santiago Pérez Torres
+// Fecha inicio: 24/10/2019
+// Última actualización: 29/12/2019
+// LogicaFake.js
+// .....................................................................
+
+package com.example.container.appcontainer;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LogicaFake {
+
+    private final String TAG = "---LogicaDebug---";
+
+    // -------------------------------------------------------------------------------
+    // -------------------------------------------------------------------------------
+    interface RespuestaAPreguntarAlgo {
+        public void respuesta(String respuesta);
+
+    } // interface
+
+
+    private String urlServidor = "http://192.168.0.115:8080/";
+
+
+    // -------------------------------------------------------------------------------------------------------
+    //                      --> obtenerContenedoresValencia() --> elCallback: Callback
+    // -------------------------------------------------------------------------------------------------------
+    public void obtenerDatosEstacionGandia(PeticionarioREST.Callback elCallback) {
+
+        PeticionarioREST elPeticionario = new PeticionarioREST();
+
+        Map<String, String> params = new HashMap<String, String>();
+        JSONObject eljson = new JSONObject(params);
+
+        elPeticionario.hacerPeticionREST("GET", this.urlServidor + "obtenerContenedoresValencia", eljson.toString(), elCallback,
+                "application/json; charset=utf-8"
+        );
+    }
+
+} // class
+
